9013ed15a2fccbcf577474283f218be156c25e96
Eduardolc12 <94937468+Eduardolc12@users.noreply.github.com>
2023-04-27 14:59:56 -0600

    Add files via upload

---
+++ ConvertorDivisa/IServicioTemperatura.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversores
+{
+    [ServiceContract]
+    internal interface IServicioTemperatura
+    {
+        [OperationContract]
+        float obtenerFahrenheit(float celsius);
+        [OperationContract]
+        float obtenerCelsius(float fahrenheit);
+    }
+}
